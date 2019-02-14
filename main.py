95d48cec8d1fd62447f0684ec781b71c6241d832
DonMin <maks000156@yandex.ru>
2019-02-14 14:38:49 +0400

    все 4 варианта реализованы

--- main.py
+++ main.py
@@ -86,16 +86,68 @@ def lineDotByDotfour(x0, y0, x1, y1, win, color):
                 y+=-1
             error -= 1
 
+def colore(r,g,b):
 
+    return r*0.2126+0.7152*g+b*0.0722
+
+
+def lineDotByDotBy(x0, y0, x1, y1, win, color):
+    steeps = False
+    if np.abs(x0 - x1)<np.abs(y0-y1):
+        x0,y0 = swap(x0,y0)
+        x1,y1 = swap(x1,y1)
+        steeps=True
+    if (x0>x1):
+        x0, x1 = swap(x0, x1)
+        y0, y1 = swap(y0, y1)
+
+    dx = x1-x0
+    dy = y1-y0
+    derror = np.abs(dy/dx)
+    print("derror ", derror)
+    error = 0
+    y=y0
+    sy = 0
+    if (y1>y0):
+        sy=1
+    else:
+        sy=-1
+
+    arr = np.arange(x0, x1, 1)
+    for i in arr:
+        if steeps:
+            point = Point(i,y)
+            point.setFill(color_rgb(int(255*(1-error)),0,0))
+            print(int(255*(1-error)))
+            point.draw(win)
+            point = Point(i, y+sy)
+            point.setFill(color_rgb(int(255*(error)),0,0))
+            print(int(255 * (error)))
+            point.draw(win)
+        else:
+            point = Point(y, i)
+            point.setFill(color_rgb(int(255*(1-error)),0,0))
+            print(int(255 * (1 - error)))
+            point.draw(win)
+            point = Point(y + sy,i)
+            point.setFill(color_rgb(int(255*(error)),0,0))
+            print(int(255 * (error)))
+            point.draw(win)
+        error+=derror
+        if (error>1):
+
+            y += sy
+
+            error -= 1
 
 def embeddedLine():
     win = GraphWin('Line', 200, 200)
 
     rect = Rectangle(Point(0, 0), Point(199, 199))
-    rect.setFill("white")
+    rect.setFill("black")
     rect.draw(win)
     line = Point(4, 10.9)
-    line.setFill("red")
+    line.setFill("white")
     line.draw(win)
     line = Line(Point(9.9, 10), Point(190, 11.9))
     line.draw(win)
@@ -104,17 +156,17 @@ def embeddedLine():
 
 def preparation(width):
     win = GraphWin('Line', width, width)
-    win.setBackground("white")
+    win.setBackground("black")
     center = width/2
     #lineDotByDot(100, 100, 150, 150, win, "green")
-    star(center, center, 20, center, win)
+    star(center, center, 13, center, win)
 
 def star(x0, y0, rayNumber, radius, win):
     start = np.pi
-    arr = np.arange(-np.pi, np.pi+0.01, np.pi/rayNumber)
+    arr = np.arange(-np.pi, np.pi, 2*np.pi/rayNumber)
     print(arr)
     for i in arr:
-        lineDotByDotfour(x0, y0, (radius*np.cos(i))+x0, (radius*np.sin(i))+y0, win, "red")
+        lineDotByDotBy(x0, y0, (radius*np.cos(i))+x0, (radius*np.sin(i))+y0, win, "white")
 
     win.getMouse()
     win.close()
