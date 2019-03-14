964aafbf9bc378c549ed3dc209bdd3ab6957651d
DonMin <maks000156@yandex.ru>
2019-03-14 19:41:18 +0400

    новые 2D координаты + вращение изображения

--- main.py
+++ main.py
@@ -2,79 +2,133 @@ import sys
 from itertools import repeat
 
 import numpy as np
-from PIL import Image , ImageDraw
+from PIL import Image, ImageDraw
 
 import painter as pt
 import triangle as tr
 from obj_parser import getPointDraw
 
 
-def get_point( index ) :
+def get_point(index):
     """
     extract 3D point from vertex_list with specified index with scale and center point, recording to main image
 
     :param index: index of point in vertex_list, starting form 0
     :return: 3D Point from triangle module
     """
-    return tr.Point( scale * vertex_list[ index , 0 ] + center ,
-                     scale * vertex_list[ index , 1 ] + center ,
-                     scale * vertex_list[ index , 2 ] + center )
+    return tr.Point(scale * vertex_list[index, 0] + center,
+                    scale * vertex_list[index, 1] + center,
+                    scale * vertex_list[index, 2] + center)
 
 
 # завел отдельный метод, потому что чтобы унифицировать с get_point, то надо будет постоянно массив передавать, а это так себе затея
 # думаю, и так нормально. Вычитать нужно, потому что он так в каком-то перевернутом виде. Установил просто эмпирически
 # texture -- картинка с текстурой, сами значения в обж лежат в диапазоне от 0 до 1, так что надо умножать на размер по каждому измерению
-def get_texture_point( index ) :
-    return tr.Point( texture.size[ 0 ] * float( texture_coordinates[ index ].first ) ,
-                     texture.size[ 1 ] -
-                     texture.size[ 1 ] * float( texture_coordinates[ index ].second ) , 0 )
-
-def Matrix_viewport(x, y, w, h) :
-
-    matrix = np.eye(4)
-    matrix[0][3] = x+w/2.
-    matrix[1][3] = y+h/2.
+def get_texture_point(index):
+    return tr.Point(texture.size[0] * float(texture_coordinates[index].first),
+                    texture.size[1] -
+                    texture.size[1] * float(texture_coordinates[index].second), 0)
 
 
-    matrix[0][0] = w/2.
-    matrix[1][1] = h/2.
+def new3DCoordinates(t, axis, angle):
+    if (axis[0] == 1):
+        alpha = angle[0]  # угол
+        R_x = np.array(np.zeros((3, 4)))  # вращение вокруг оси х
+        R_x[0, 0] = 1
+        R_x[1, 1] = np.cos(alpha)
+        R_x[2, 2] = np.cos(alpha)
+        R_x[1, 2] = (-1) * np.sin(alpha)
+        R_x[2, 1] = np.sin(alpha)
+        R_x[:, 3] = t
 
-    return matrix
+        for i in range(len(vertex_list)):
+            M = np.array([vertex_list[i, 0], vertex_list[i, 1], vertex_list[i, 2], 1])
+            tmp = R_x.dot(M)
+            vertex_list[i, 0] = tmp[0]
+            vertex_list[i, 1] = tmp[1]
+            vertex_list[i, 2] = tmp[2]
+
+    if (axis[1] == 1):  # вращение вокруг оси y
+        alpha = angle[1]  # угол
+        R_y = np.array(np.zeros((3, 4)))
+        R_y[0, 0] = np.cos(alpha)
+        R_y[1, 1] = 1
+        R_y[2, 2] = np.cos(alpha)
+        R_y[0, 2] = np.sin(alpha)
+        R_y[2, 0] = (-1) * np.sin(alpha)
+        R_y[:, 3] = t
+
+        for i in range(len(vertex_list)):
+            M = np.array([vertex_list[i, 0], vertex_list[i, 1], vertex_list[i, 2], 1])
+            tmp = R_y.dot(M)
+            vertex_list[i, 0] = tmp[0]
+            vertex_list[i, 1] = tmp[1]
+            vertex_list[i, 2] = tmp[2]
+
+    if (axis[2] == 1):  # вращение вокруг оси z
+        alpha = angle[2]  # угол
+        R_z = np.array(np.zeros((3, 4)))
+        R_z[0, 0] = np.cos(alpha)
+        R_z[1, 1] = np.cos(alpha)
+        R_z[2, 2] = 1
+        R_z[0, 1] = (-1) * np.sin(alpha)
+        R_z[1, 0] = np.sin(alpha)
+        R_z[:, 3] = t
+
+        for i in range(len(vertex_list)):
+            M = np.array([vertex_list[i, 0], vertex_list[i, 1], vertex_list[i, 2], 1])
+            tmp = R_z.dot(M)
+            vertex_list[i, 0] = tmp[0]
+            vertex_list[i, 1] = tmp[1]
+            vertex_list[i, 2] = tmp[2]
+
+    return vertex_list
 
 
+def setParametrsForProjection():
+    camera_offset = 15
+    f_u = 1500
+    f_v = 1500
+    u_0 = 800
+    v_0 = 800
+    return camera_offset, f_u, f_v, u_0, v_0
 
-if __name__ == '__main__' :
-    im = Image.new( 'RGB' , (1000 , 1000) , color=(255 , 255 , 255 , 0) )
-    texture = Image.open( 'african_head_diffuse.png' )
-    texture = texture.convert( 'RGB' )
-    width , height = texture.size
 
-    draw = ImageDraw.Draw( im )
+if __name__ == '__main__':
+    im = Image.new('RGB', (1000, 1000), color=(255, 255, 255, 0))
+    texture = Image.open('african_head_diffuse.png')
+    texture = texture.convert('RGB')
+    width, height = texture.size
+
+    draw = ImageDraw.Draw(im)
     # scale = - im.width / 2 * .9
     # center = im.width / 2
     scale = -1
     center = 0
-    vertex_list , edges_list_with_text , texture_coordinates = getPointDraw( 'african_head.obj' )
+    vertex_list, edges_list_with_text, texture_coordinates = getPointDraw('african_head.obj')
+    vertex_list = new3DCoordinates(t=[0, 0, 4], axis=[1, 1, 1], angle=[45, 90, 45])
 
-    color = (255 , 255 , 255)
-    cam_direction = tr.Point( 0 , 0, 1 )
-    light_direction = tr.Point( 0 , 0 , 1 )
-    z_buffer = np.array( list( repeat( sys.maxsize , im.width * im.height ) ) )
-    z_buffer.shape = (im.width , im.height)
-    trianglesWithCoords = [ tr.Triangle( get_point( int( triangle.first ) - 1 ) ,
-                                         get_point( int( triangle.second ) - 1 ) ,
-                                         get_point( int( triangle.third ) - 1 ) ,
-                                         get_texture_point( int( triangle.textureFirst ) - 1 ) ,
-                                         get_texture_point( int( triangle.textureSecond ) - 1 ) ,
-                                         get_texture_point( int( triangle.textureThird ) - 1 ) ) for triangle in edges_list_with_text ]
+    color = (255, 255, 255)
+    cam_direction = tr.Point(0, 0, 1)
+    light_direction = tr.Point(0, 0, 1)
+    z_buffer = np.array(list(repeat(sys.maxsize, im.width * im.height)))
+    z_buffer.shape = (im.width, im.height)
+    trianglesWithCoords = [tr.Triangle(get_point(int(triangle.first) - 1),
+                                       get_point(int(triangle.second) - 1),
+                                       get_point(int(triangle.third) - 1),
+                                       get_texture_point(int(triangle.textureFirst) - 1),
+                                       get_texture_point(int(triangle.textureSecond) - 1),
+                                       get_texture_point(int(triangle.textureThird) - 1)) for triangle in
+                           edges_list_with_text]
 
-    for triangle in filter( lambda polygon : polygon.direction().angle( cam_direction ) > 0 ,
-                            trianglesWithCoords ) :
-        light_angle = triangle.direction().angle( light_direction )
+    for triangle in filter(lambda polygon: polygon.direction().angle(cam_direction) > 0,
+                           trianglesWithCoords):
+        light_angle = triangle.direction().angle(light_direction)
 
         # Вообще говоря, для отрисовки треугольников color больше не нужен, поскольку цвет берется из файла текстур,
         # но раз уж у нас остается функционал отрисовки линий, то передавать цвет все равно нужно
-        pt.paint_triangle( triangle , draw , z_buffer , light_angle , texture , color=tuple( [ int( i * light_angle ) for i in list( color ) ] ) ,
-                           lines=False)
+        pt.paint_triangle(triangle, draw, z_buffer, light_angle, texture,
+                          color=tuple([int(i * light_angle) for i in list(color)]),
+                          lines=False)
 
-    im.save( 'african_head.png' )
+    im.save('african_head.png')
--- triangle.py
+++ triangle.py
@@ -1,76 +1,73 @@
 import math
-
+import main
 import graphics
 import numpy as np
 
 
-class Point :
+class Point:
     """
     Class described 3D point or 3D vector
     """
 
-    def __init__( self , first: float , second: float , third: float ) :
+    def __init__(self, first: float, second: float, third: float):
         self.first = first
         self.second = second
         self.third = third
 
-    def __to_array__( self ) :
-        return [ self.first , self.second , self.third ]
+    def __to_array__(self):
+        return [self.first, self.second, self.third]
 
-    def dot_product( self , another ) -> float :
-        return sum( i * j for i , j in zip( self.__to_array__() , another.__to_array__() ) )
+    def dot_product(self, another) -> float:
+        return sum(i * j for i, j in zip(self.__to_array__(), another.__to_array__()))
 
-    def norm( self ) -> float :
-        return math.sqrt( self.dot_product( self ) )
+    def norm(self) -> float:
+        return math.sqrt(self.dot_product(self))
 
-    def angle( self , another ) -> float :
+    def angle(self, another) -> float:
         """
         Calculate cos of angle with another vector
         :param another: vector to multiply
         :return: cos of angle
         """
-        return self.dot_product( another ) / (self.norm() * another.norm())
+        return self.dot_product(another) / (self.norm() * another.norm())
 
-    def vector_product( self , another ) :
-        cross = np.cross( self.__to_array__() , another.__to_array__() )
-        return Point( cross[ 0 ] , cross[ 1 ] , cross[ 2 ] )
+    def vector_product(self, another):
+        cross = np.cross(self.__to_array__(), another.__to_array__())
+        return Point(cross[0], cross[1], cross[2])
 
-    def subtract( self , another ) :
-        subtract = np.subtract( self.__to_array__() , another.__to_array__() )
-        return Point( subtract[ 0 ] , subtract[ 1 ] , subtract[ 2 ] )
+    def subtract(self, another):
+        subtract = np.subtract(self.__to_array__(), another.__to_array__())
+        return Point(subtract[0], subtract[1], subtract[2])
 
-    def projection( self ) -> graphics.Point :
+    def projection(self) -> graphics.Point:
         """
         Convert to 2D point, x and y axis.
         Need rotation method
 
         :return: 2D Point from graphics module
         """
-        f = 40000
-        w = 500
-        h = 500
-        M = np.array([ self.first , self.second,  self.third + 5  ,1])
-        K = np.zeros((3,4))
-        K[0][0]= 1500
-        K[1][1]= 1500
-        K[2][2]=1
-        K[0][2]= 500
-        K[1][2]= 500
+        camera_offset, f_u, f_v, u_0, v_0 = main.setParametrsForProjection()
+        M = np.array([self.first, self.second, self.third + camera_offset, 1])
+        K = np.zeros((3, 4))
+        K[0][0] = f_u
+        K[1][1] = f_v
+        K[2][2] = 1
+        K[0][2] = u_0
+        K[1][2] = v_0
         m = K.dot(M)
-        m /=m[2]
+        m /= m[2]
         self.first = m[0]
         self.second = m[1]
         self.third = 1
-        return graphics.Point( self.first, self.second)
+        return graphics.Point(self.first, self.second)
 
 
-
-class Triangle :
+class Triangle:
     """
     Describes triangle polygon
     """
 
-    def __init__( self , first: Point , second: Point , third: Point , textureFirst=0 , textureSecond=0 , textureThird=0 ) :
+    def __init__(self, first: Point, second: Point, third: Point, textureFirst=0, textureSecond=0, textureThird=0):
         self.first = first
         self.second = second
         self.third = third
@@ -78,16 +75,16 @@ class Triangle :
         self.textureSecond = textureSecond
         self.textureThird = textureThird
 
-    def direction( self ) -> Point :
+    def direction(self) -> Point:
         """
          Calculate by vector product vector, shows direction of polygon
         :return: 3D direction vector
         """
-        first_vec = self.first.subtract( self.second )
-        second_vec = self.first.subtract( self.third )
-        return first_vec.vector_product( second_vec )
+        first_vec = self.first.subtract(self.second)
+        second_vec = self.first.subtract(self.third)
+        return first_vec.vector_product(second_vec)
 
-    def bilinear_interpolation( self , point: graphics.Point ) -> float :
+    def bilinear_interpolation(self, point: graphics.Point) -> float:
         """
         Calculate z depth of 2D point projection in 3D triangle polygon. Method recording to surface
         equation http://www.mathprofi.ru/uravnenie_ploskosti.html#ou
@@ -95,35 +92,41 @@ class Triangle :
         :param point: 2D point inside triangle projection to x-y surface, where need calculate z depth
         :return: z depth
         """
-        first , second , third = self.first , self.second , self.third
+        first, second, third = self.first, self.second, self.third
 
-        x0 , y0 , z0 = first.first , first.second , first.first
-        x1 , y1 , z1 = second.first , second.second , second.first
-        x2 , y2 , z2 = third.first , third.second , third.first
-        x , y = point.x , point.y
+        x0, y0, z0 = first.first, first.second, first.first
+        x1, y1, z1 = second.first, second.second, second.first
+        x2, y2, z2 = third.first, third.second, third.first
+        x, y = point.x, point.y
         divider = ((x1 - x0) * (y2 - y0) - (x2 - x0) * (y1 - y0))
         return z0 \
                + (x - x0) * (((y2 - y0) * (z1 - z0) - (y1 - y0) * (z2 - z0)) / divider) \
                + (y - y0) * (((x1 - x0) * (z2 - z0) - (x2 - x0) * (z1 - z0)) / divider)
 
-    def is_inside_triangle( self , point: graphics.Point ) -> bool :
+    def is_inside_triangle(self, point: graphics.Point) -> bool:
         """
         Checks, if triangle, specified by 3 points, contains point. Doesn't record to 3D
 
         :param point: gr.Point to check
         :return: True, if triangle contains point inside
         """
-        l0 , l1 , l2 = self.getBaricenterCordinates( point )
+        l0, l1, l2 = self.getBaricenterCordinates(point)
         return l0 >= 0 and l1 >= 0 and l2 >= 0
 
     # Вынесли в отдельный метод, потому что для текстур нужно не только проверить условие, но и сами координаты
-    def getBaricenterCordinates( self , point: graphics.Point ) :
-        first , second , third = self.first , self.second , self.third
-        l0 = ((point.y - third.second) * (second.first - third.first) - (point.x - third.first) * (second.second - third.second)) / \
-             ((first.second - third.second) * (second.first - third.first) - (first.first - third.first) * (second.second - third.second))
-        l1 = ((point.y - first.second) * (third.first - first.first) - (point.x - first.first) * (third.second - first.second)) / \
-             ((second.second - first.second) * (third.first - first.first) - (second.first - first.first) * (third.second - first.second))
-        l2 = ((point.y - second.second) * (first.first - second.first) - (point.x - second.first) * (first.second - second.second)) / \
-             ((third.second - second.second) * (first.first - second.first) - (third.first - second.first) * (first.second - second.second))
+    def getBaricenterCordinates(self, point: graphics.Point):
+        first, second, third = self.first, self.second, self.third
+        l0 = ((point.y - third.second) * (second.first - third.first) - (point.x - third.first) * (
+                    second.second - third.second)) / \
+             ((first.second - third.second) * (second.first - third.first) - (first.first - third.first) * (
+                         second.second - third.second))
+        l1 = ((point.y - first.second) * (third.first - first.first) - (point.x - first.first) * (
+                    third.second - first.second)) / \
+             ((second.second - first.second) * (third.first - first.first) - (second.first - first.first) * (
+                         third.second - first.second))
+        l2 = ((point.y - second.second) * (first.first - second.first) - (point.x - second.first) * (
+                    first.second - second.second)) / \
+             ((third.second - second.second) * (first.first - second.first) - (third.first - second.first) * (
+                         first.second - second.second))
 
-        return l0 , l1 , l2
+        return l0, l1, l2
